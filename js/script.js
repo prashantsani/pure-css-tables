378fa9ad3592e9dc874b60dad54d822f665011d6
Prashant Sani <contact@prashantsani.com>
2019-07-26 18:00:25 +0530

    basic JSON --> HTML

--- js/script.js
+++ js/script.js
@@ -47,9 +47,38 @@ Modernizr.on('webp', function(result) {
 	Plugins
 ------------ */
 
-// w = window;
-// d = document;
-(function(w,d){
-	
+ w = window;
+ d = document;
+//(function(w,d){
 
-})(window,document);
+	var _html = d.documentElement,
+			request = new XMLHttpRequest(),
+			data = undefined,
+			url = 'helpers/dummy-json.json',
+			template_list = Handlebars.compile(d.getElementById('template-list').innerHTML),
+			_$ = function (selector){
+				return d.querySelectorAll(selector);
+			},
+			customer_list = _$('#customer-list')[0];
+
+
+	// Ajaxing data
+	request.open('GET', url, true);
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+				// Success!!
+				data = JSON.parse(request.responseText);
+				console.log(data)
+
+				customer_list.innerHTML = template_list(data)
+				
+		} else {
+				data = 'We reached our target server, but it returned an error'
+		}
+	};
+	request.onerror = function() {
+		data =  'Ajax Error'
+	};
+	request.send();
+
+//})(window,document);
