a3b7373f90208816a8d56a2048f072b9ab2f92ea
Prashant Sani <contact@prashantsani.com>
2019-07-29 11:06:26 +0530

    adding redundant JS code

--- js/script.js
+++ js/script.js
@@ -5,47 +5,7 @@
 	https://prashantsani.com
 
 */
-function whichAnimationEvent(){
-	var t,
-		el = document.createElement("fakeelement");
 
-	var animations = {
-		"animation"      : "animationend",
-		"OAnimation"     : "oAnimationEnd",
-		"MozAnimation"   : "animationend",
-		"WebkitAnimation": "webkitAnimationEnd"
-	}
-
-	for (t in animations){
-		if (el.style[t] !== undefined){
-		return animations[t];
-		}
-	}
-}
-var animationEvent = whichAnimationEvent();
-
-
-/* ------------
-	Feature Detection
------------- */
-// Fallback for SVG
-if (!Modernizr.svg) {
-  $('img[src*="svg"]').attr('src', function() {
-      return $(this).attr('src').replace('.svg', '.png');
-  });
-}
-// Fallback for WEBP
-Modernizr.on('webp', function(result) {
-  if (result) {
-    console.log('Webp Supported');
-  } else {
-    console.log('Webp NOT Supported');
-  }
-});
-
-/* ------------
-	Plugins
------------- */
 
 (function(w,d){
 
